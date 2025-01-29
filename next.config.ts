bba3780f162ae060e5de53c24763255060db5e93
AlbanFK <fokoalban3@gmail.com>
2025-01-29 15:35:02 +0100

    comment i18n config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
-import nextI18nextConfig from "./next-i18next.config.ts";
+// import nextI18nextConfig from "./next-i18next.config";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  i18n: nextI18nextConfig.i18n,
+  // i18n: nextI18nextConfig.i18n,
   images: {
     remotePatterns: [
       {
